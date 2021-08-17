e6510c0abb9814eaccf1e2edbcd2a653c7fc4496
JCh6 <juanch@outlook.com>
2021-08-17 16:20:05 -0500

    Add api package

---
+++ userservice/src/main/java/com/juanchaparro/userservice/api/UserResource.java
@@ -0,0 +1,25 @@
+package com.juanchaparro.userservice.api;
+
+import com.juanchaparro.userservice.domain.User;
+import com.juanchaparro.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserResource {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+
+
+}
