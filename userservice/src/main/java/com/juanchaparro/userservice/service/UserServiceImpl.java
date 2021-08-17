973581b965a6fd49e0afc3366deb63c21b85b911
JCh6 <juanch@outlook.com>
2021-08-17 15:10:19 -0500

    Add domain, repo and service package

---
+++ userservice/src/main/java/com/juanchaparro/userservice/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.juanchaparro.userservice.service;
+
+import com.juanchaparro.userservice.domain.Role;
+import com.juanchaparro.userservice.domain.User;
+import com.juanchaparro.userservice.repo.RoleRepo;
+import com.juanchaparro.userservice.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService{
+
+    private final UserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return null;
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+
+    }
+
+    @Override
+    public User getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+}
